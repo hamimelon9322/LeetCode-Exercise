e167d70e873de0f9aa9d9bbd8c99b1d5a071181e
hamimelon9322 <49954717+hamimelon9322@users.noreply.github.com>
2023-04-17 21:16:41 +0800

    Update JZ58-II.左旋转字符串-只能用String.java

--- 图解算法数据结构/JZ58-II 左旋转字符串/JZ58-II.左旋转字符串-只能用String.java
+++ 图解算法数据结构/JZ58-II 左旋转字符串/JZ58-II.左旋转字符串-只能用String.java
@@ -1,3 +1,5 @@
+// 不像StringBuilder是可变对象，每轮遍历拼接字符时，只是向列表尾部添加一个新的字符元素，最终拼接转化为字符串时，系统仅申请一次内存；
+// 而String字符串是 “不可变对象”，每轮遍历拼接字符时，都需要新建一个字符串，系统需申请N次内存 ，数据量较大时效率低下。
 class Solution {
     public String reverseLeftWords(String s, int n) {
         String res = "";
