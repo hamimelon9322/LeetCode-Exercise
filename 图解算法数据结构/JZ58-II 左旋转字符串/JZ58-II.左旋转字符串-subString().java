37443acc7ff05fb4e3aa2e651982ca5edf9e0c8e
hamimelon9322 <49954717+hamimelon9322@users.noreply.github.com>
2023-04-17 21:22:27 +0800

    Update JZ58-II.左旋转字符串-subString().java

--- 图解算法数据结构/JZ58-II 左旋转字符串/JZ58-II.左旋转字符串-subString().java
+++ 图解算法数据结构/JZ58-II 左旋转字符串/JZ58-II.左旋转字符串-subString().java
@@ -1,5 +1,6 @@
 class Solution {
     public String reverseLeftWords(String s, int n) {
         return s.substring(n, s.length()) + s.substring(0, n);// 切片函数substring() 方法返回字符串的子字符串,(beginIndex,endIndex)，开始索引（包括）从0开始，和结束索引（不包括）
+        // return (s + s).substring(n, n + s.length()); // 一个效果
     }
 }
